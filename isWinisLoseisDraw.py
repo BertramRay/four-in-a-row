5d66a4a975ef5da14d26efb60e284d44998a4e86
sherlockforever <64008001+sherlockforever@users.noreply.github.com>
2020-04-30 13:26:56 +0800

    Update isWinisLoseisDraw.py

--- isWinisLoseisDraw.py
+++ isWinisLoseisDraw.py
@@ -1,27 +1,25 @@
 def isWin( gameStateBoard, x , y , color):
     # 参数当前状态gameState
-    # 参数当前状态x , y为新棋子的位置，color为新棋子颜色
+    # 参数当前状态x , y是action为新棋子的位置，color为新棋子颜色
     # 返回bool类型，是否当前颜色胜利
     # 还可以改进比如剩余距离和当前连在一起数量小于4，可不查！
     # 检查竖着的是否有四个以上连着
     number_continuuum = 1 #已经计入新的棋子
-    t = 0
-    for i in range( y + 1, 5, 1) :#i代表列
-        if gameStateBoard[i][y] != color:
-            t = i 
-            break
-    number_continuuum += ( t - x ) 
+    t=x+1
+    while t<=5 and gameStateBoard[t][y] != color:#t代表行
+        t+=1
+    number_continuuum += ( t-x-1 ) 
     if number_continuuum >= 4 :
         return True
 
     #检查横着的是否有四个以上连着
     number_continuuum = 1 #已经计入新的棋子
-    for j in range( y + 1, 6, 1) :#j代表行
+    for j in range( y + 1, 7, 1) :#j代表行
         if gameStateBoard[x][j] != color:
             t = j 
             break
-    number_continuuum += ( t - y )
-    for j in range( y - 1, 0, -1) :#j代表行
+    number_continuuum += ( t - y-1 )
+    for j in range( y - 1, -1, -1) :#j代表行
         if gameStateBoard[x][j] != color:
             t = j 
             break
@@ -31,7 +29,7 @@ def isWin( gameStateBoard, x , y , color):
 
     #左上-右下
     number_continuuum = 1#已经计入新的棋子
-    k = 1 
+    k = 1
     while ( x - k ) >= 0 and ( y - k ) >= 0:
         if gameStateBoard[x - k][y - k] != color:
             break
@@ -48,7 +46,7 @@ def isWin( gameStateBoard, x , y , color):
 
     #左下-右上
     number_continuuum = 1#已经计入新的棋子
-    k = 1 
+    k = 1    
     while ( x - k ) >= 0 and ( y + k ) <= 6:
         if gameStateBoard[x - k][y + k] != color:
             break
@@ -65,7 +63,6 @@ def isWin( gameStateBoard, x , y , color):
     
     #不属于上述任何获胜的可能情况
     return False
-
 
 def isDraw( gameStateBoard , x , y , color ):
     # 参数当前状态gameState
